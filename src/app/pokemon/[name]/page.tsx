2ab541c75e3ee58d769a1abc8245d87f6c9dcae8
Daniyal Aslam <daniyalaslamdeveloper@gmail.com>
2025-08-14 16:01:41 +0500

    Fix: Pokemon Details Page.

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,6 +1,9 @@
  
 import PokemonDetailPageClient from '@/features/pokemon/components/PokemonDetailPageClient';
 
-export default function PokemonDetailPage({ params }: { params: { name: string } }) {
+type PokemonPageProps = {
+  params: { name: string };
+};
+export default function PokemonDetailPage({ params }: PokemonPageProps) {
   return <PokemonDetailPageClient name={params.name} />;
 }
