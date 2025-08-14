4ae7c0927374fd3ff5019542c27fc1b0f8ab815b
Daniyal Aslam <daniyalaslamdeveloper@gmail.com>
2025-08-14 15:40:01 +0500

    Fix: Base URL Issue

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_POKEMON_API,
+  baseURL: process.env.NEXT_PUBLIC_POKEMON_API || 'https://pokeapi.co/api/v2',
 });
 
 export default api;
