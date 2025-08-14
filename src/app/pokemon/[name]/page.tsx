8542b74b84539b3b0f6951774d288e99a415850c
Daniyal Aslam <daniyalaslamdeveloper@gmail.com>
2025-08-14 17:06:41 +0500

    Fix: Type Error Issue

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,10 +1,24 @@
 import PokemonDetailPageClient from '@/features/pokemon/components/PokemonDetailPageClient';
+import { Metadata } from 'next';
 
-interface PokemonPageProps {
+interface PageProps {
   params: { name: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
+}
+ 
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const name = params.name;
+
+  return {
+    title: `${name} | Pokémon Details`,
+    description: `View detailed information about ${name}, including types, abilities, stats, and more.`,
+    openGraph: {
+      title: `${name} | Pokémon Details`,
+      description: `Detailed stats and info about Pokémon ${name}.`,
+      url: `/pokemon/${name}`,
+    },
+  };
 }
 
-export default function PokemonDetailPage({ params }: PokemonPageProps) {
+export default function PokemonDetailPage({ params }: PageProps) { 
   return <PokemonDetailPageClient name={params.name} />;
 }
